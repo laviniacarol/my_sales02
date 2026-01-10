2c3f9a7d3841366f3a7e0998a6b036ee3f606e59
laviniacarol <carolxlavinia@gmail.com>
2026-01-09 21:42:15 -0300

    table migrations and products :p

---
+++ src/shared/typeorm/migrations/1768004873236-%npm_config_name%.ts
@@ -0,0 +1,51 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateMigration1768004873236 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "products",
+        columns: [{
+           name: "id",
+           type: "int",
+           isPrimary: true,
+           isGenerated: true,
+           generationStrategy: "increment"
+          },
+          {
+             name: "name",
+             type: "varchar"
+            },
+            {
+              name: "price",
+              type: "decimal",
+              precision: 10,
+              scale: 2
+            },
+            {
+              name: "quantity",
+              type: "int"
+            },
+            {
+              name: "created_at",
+              type: "timestamp",
+              default: "now()"
+            },
+            {
+               name: "updated_at",
+                type: "timestamp",
+                default: "now()"
+              }
+            ]
+
+      }
+
+      )
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+  }
+
+}
